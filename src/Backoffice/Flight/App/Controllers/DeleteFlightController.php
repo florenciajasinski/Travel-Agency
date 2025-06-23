a5bf22f4d997059c6dfbe9b37a2949c17fedf049
Florencia Jasinski <florenciajasinski@Florencias-MacBook-Pro.local>
2025-06-23 15:14:06 -0300

    fix: fix format

--- src/Backoffice/Flight/App/Controllers/DeleteFlightController.php
+++ src/Backoffice/Flight/App/Controllers/DeleteFlightController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Lightit\Backoffice\Flight\App\Controllers;
 
 use Illuminate\Http\JsonResponse;
-use Lightit\Backoffice\Flight\App\Resources\FlightResource;
 use Lightit\Backoffice\Flight\Domain\Actions\DeleteFlightAction;
 use Lightit\Backoffice\Flight\Domain\Models\Flight;
 
