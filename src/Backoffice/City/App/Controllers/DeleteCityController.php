e997fb86866acc53848f9c491961bb6de8cb1e37
Florencia Jasinski <florenciajasinski@Florencias-MacBook-Pro.local>
2025-06-23 12:09:39 -0300

    feat: add url and delete

---
+++ src/Backoffice/City/App/Controllers/DeleteCityController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Backoffice\City\App\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Lightit\Backoffice\City\Domain\Actions\DeleteCityAction;
+use Lightit\Backoffice\City\Domain\Models\City;
+
+class DeleteCityController
+{
+    public function __invoke(DeleteCityAction $deleteCityAction, City $city): JsonResponse
+    {
+        $deleteCityAction->execute($city);
+
+        return response()->json([
+            'message' => 'City deleted successfully.',
+        ]);
+    }
+}
---
+++ src/Backoffice/City/Domain/Actions/DeleteCityAction.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Backoffice\City\Domain\Actions;
+use Lightit\Backoffice\City\Domain\Models\City;
+
+class DeleteCityAction
+{
+    public function execute(City $city): void
+    {
+        $city->delete();
+    }
+}
