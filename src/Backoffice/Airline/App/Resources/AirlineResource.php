fe77d8677b24377bb77030c6e6067fda88d60189
Florencia Jasinski <florenciajasinski@Florencias-MacBook-Pro.local>
2025-07-01 12:28:03 -0300

    fix: fix AirlineResource

--- src/Backoffice/Airline/App/Resources/AirlineResource.php
+++ src/Backoffice/Airline/App/Resources/AirlineResource.php
@@ -15,9 +15,9 @@ class AirlineResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->resource->id ?? null,
-            'name' => $this->resource->name ?? null,
-            'description' => $this->resource->description ?? null,
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'description' => $this->resource->description,
             'flights_count' => $this->resource->flights_count ?? 0,
         ];
     }
